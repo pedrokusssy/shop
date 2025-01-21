34d909f943adf3575518c6a76ece2a0599a956b0
Pedro Kusisakana <49099181+pedrokusssy@users.noreply.github.com>
2025-01-21 03:14:20 +0000

    removed RouterOutlet from imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { InputComponent } from "../component/input/input.component";
 import { ListComponent } from '../component/list/list.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, InputComponent, ListComponent],
+  imports: [ InputComponent, ListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
