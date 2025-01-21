ba978b6943fcc8f88e9832b28010fec794a1f20e
Pedro Kusisakana <49099181+pedrokusssy@users.noreply.github.com>
2025-01-21 03:10:05 +0000

    input component with dependece inject from listServive Class

--- src/component/input/input.component.ts
+++ src/component/input/input.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ListService } from '../../service/list.service';
 
 @Component({
   selector: 'app-input',
@@ -7,5 +8,13 @@ import { Component } from '@angular/core';
   styleUrl: './input.component.css'
 })
 export class InputComponent {
+  private isAdded: boolean = false;
+
+  addItem(item: any){
+    this.isAdded = this._listService.add(item.value);
+    if(this.isAdded)item .value= ""
+  }
+
+  constructor(private _listService: ListService){}
 
 }
