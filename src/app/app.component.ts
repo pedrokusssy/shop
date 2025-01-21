ca9bd302b1f9ee8807269f6ad6c8ad6f17bef915
Pedro Kusisakana <49099181+pedrokusssy@users.noreply.github.com>
2025-01-21 01:44:36 +0000

    input component and list component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { InputComponent } from "../component/input/input.component";
+import { ListComponent } from '../component/list/list.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, InputComponent, ListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
