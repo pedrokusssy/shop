02d0452d35d222d837a0edcb04bdf3b114384842
Pedro Kusisakana <49099181+pedrokusssy@users.noreply.github.com>
2025-01-21 03:11:09 +0000

    list component with DI - dependence injection from serviceList

--- src/component/list/list.component.ts
+++ src/component/list/list.component.ts
@@ -1,11 +1,22 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
+import { ListService } from '../../service/list.service';
 
 @Component({
   selector: 'app-list',
-  imports: [],
+  imports: [NgFor],
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
 export class ListComponent {
+
+ private items: string[] = []
+
+  constructor(private _lsita: ListService){
+    this.items = this._lsita.get();
+  }
+  getItems(){
+    return this.items;
+  }
 
 }
